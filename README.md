b58ca447125d0df50babc80a156029c9375969f1
Deivismdev <deivismdev@gmail.com>
2021-10-06 22:22:54 +0300

    reimplimented multithreading (correctly this time)

--- README.md
+++ README.md
@@ -4,31 +4,38 @@ A GUI program for easy downloading videos from youtube and saving them as mp3's 
 Made using python, pytube and tkinter. <br />
 
 ## How to use?
-Just paste url's in the textbox, separated by spaces or new lines and click Download  <br />
+Just paste url's in the textbox, separated by spaces or new lines and click Download button  <br />
 
 ## Example:
+Running the program<br />
+<img src="Images\image0.png"/> <br />
 Clicking Download button without pasting any url's or pasting url's not from youtube videos will result in a message showing up "PASTE URL'S FROM YOUTUBE":  <br />
 <img src="Images\image-1.png"/> <br />
-Example of one of the ways to paste url's<br />
-<img src="Images\image0.png"/> <br />
-After clicking Download, files will be downloaded and saved into a directory named "downloaded files" in the same directory where program is located<br />
-NOTE: program might freeze up while files are being downloaded. Work in progress!<br />
+Pasting url's and clicking Download button will result in a message showing up "DOWNLOADING..." and progress bar moving<br />
 <img src="Images\image1.png"/> <br />
+Files will be downloaded and saved into a directory named "downloaded files" in the same directory where program is located<br />
+<img src="Images\image3.png"/> <br />
 
-## Having errors?
+## Having errors when running the code?
 Make sure to have pytube and tkinter installed 
 <pre><code>pip install pytube 
 pip install tkinter 
 </code></pre>
-Do not skip the first line when pasting url's
-## Program crashing?
-I know :( <br />
-Work in progress... <br /> 
-For now try not to drag the window while downloading<br />
+
 
 ## Future updates
 * Ability to choose where files will be saved <br />
 * Ability to choose between downloading mp3's and mp4's (or both) <br />
 * <span style="color:green">Downloading files in parallel (DONE!)<br /></span>
-* Progress bar <br />
-* Fix crashing when dragging the window <br />
+* <span style="color:yellow"> Progress bar (kinda done)<br />
+* <span style="color:green">Fix crashing when dragging the window (DONE!) <br /></span>
+
+## Change log
+2021-09-22 v1.0 <br />
+First release  <br />
+2021-09-22 v1.1 <br />
+Downloaded mp3's are now saved into a separate directory. Code clean up  <br />
+2021-09-27 v1.2 <br />
+Videos download faster using multithreading. Program still crashes because Tkinter is single-threaded :(.<br />
+2021-10-06 v2.0 <br />
+Reimplimented multithreading (correctly this time), downloading is fast and program no longer crashes! Added progress bar + label to indicate downloading process. New logo.
--- yt_to_mp3.py
+++ yt_to_mp3.py
@@ -1,72 +1,83 @@
 import os
 import tkinter as tk
-from tkinter.constants import END, NORMAL
+from tkinter.constants import END
 from pytube import YouTube
-import concurrent.futures
-import time
+import threading
+import tkinter.ttk as ttk
 
 program_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(program_path)
 download_dir = 'Downloaded files'
 
+# creates directory for downloeded files
 if not os.path.exists(download_dir):
     os.makedirs(download_dir)
 os.chdir(download_dir)
 
-def convertTextToList():
+# Puts input from textbox to a list
+def convertInputToList():
     all_urls = textBox.get('1.0',END).strip().split('\n')
-    if(all_urls[0]) == '': # checks if textBox is empty (not perfect since first line can be skipped) maybe fix this
+    if all_urls[0] == '':
         label_status.config(text='PASTE URL\'S', bg='#ff9999')
-        return
-    download_button.config(state=NORMAL)
-    return all_urls
+    # this if else feels wrong
+    else:
+        return all_urls
 
+# Downloads the video, changes extension to .mp3
 def download(url):
     try:
+        label_status.config(text='DOWNLOADING...', bg='#FFFF00')
+        progress_bar.start()
         video = YouTube(url)
         video = video.streams.get_by_itag(251)
         outfile = video.download()
-
-        base, ext = os.path.splitext(outfile)
+        base, _ = os.path.splitext(outfile)
         new_file = base + '.mp3'
         if not(os.path.exists(new_file)):
             os.rename(outfile, new_file)
     except Exception:
         label_status.config(text='PASTE URL\'S FROM YOUTUBE', bg='#ff9999')
-    
-# ay multithreading!
-def executor():
+
+    label_status.config(text='DOWNLOAD COMPLETE', bg='#4bc449')
+    progress_bar.stop()
+
+# Creates thread for each url
+def createThreads(urls):
     try:
-        t1 = time.perf_counter()
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(download, convertTextToList()) 
-        t2 = time.perf_counter()
-        print(f'Finished in {t2-t1} seconds')
+        for url in urls:
+            downloadThread=threading.Thread(target=lambda:download(url))
+            downloadThread.start()
     except Exception:
         label_status.config(text='PASTE URL\'S FROM YOUTUBE', bg='#ff9999')
 
 window = tk.Tk()
 window.title("Youtube to mp3")
-window.geometry('450x300')
+window.geometry('600x300')
 window.iconbitmap('..\Images\icon.ico')
 
-# instruction label
+# Instruction label
 instruction_label = tk.Label(window, text='Paste URL\'s below: (separate with newlines or spaces)',font=('century gothic',10))
 instruction_label.grid(row=0,column=0,columnspan=3)
 
-# textbox for pasting url's
+# Textbox for pasting url's
 textBox = tk.Text(window)
 textBox.grid(row=1,column=0,sticky=(tk.N, tk.S, tk.E, tk.W),columnspan=3)
 
-# status / instruction / error label
+# Status / instruction / error label
 label_status = tk.Label(window, font=('century gothic',10))
 label_status.grid(row=2,column=0,sticky=(tk.N, tk.S, tk.E, tk.W),columnspan=3)
 
-# download button
-download_button = tk.Button(text='Download', font=('century gothic',15), command=executor)
+# Download button
+download_button = tk.Button(text='Download', font=('century gothic',15), command=lambda:createThreads(convertInputToList())) 
 download_button.grid(row=3,column=0,sticky=(tk.N, tk.S, tk.E, tk.W),columnspan=3)
 
-# scaling with window
+# Progress bar
+s = ttk.Style()
+s.theme_use('clam')
+progress_bar = ttk.Progressbar(window,mode='indeterminate')        
+progress_bar.grid(row=4, column=0,sticky=(tk.N, tk.S, tk.E, tk.W),columnspan=3)    
+
+# Scaling with window
 window.columnconfigure(1, weight=1)
 window.columnconfigure(1, weight=1)
 window.columnconfigure(2, weight=1)
